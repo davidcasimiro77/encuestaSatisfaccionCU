206fdb5fbe892a1b2c8f8da00a6b945f9e847a02
davidcasimiro77 <casimiroherreradavid2@gmail.com>
2023-06-06 10:26:16 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>¡Valóranos!</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='styles.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+    <h1>¡Valora el servicio recibido!</h1>
+    <form method='post' action=''>
+        <label>
+            <img src='images/triste.png'/>
+            <input type='submit' name='triste'/>
+        </label>
+        <label>
+            <img src='images/confuso.png'/>
+            <input type='submit' name='confuso'/>
+        </label>
+        <label>
+            <img src='images/feliz.png'/>
+            <input type='submit' name='feliz'/>
+        </label>
+    </form>
+</body>
+</html>
